6d3b8647d8c1411ef5d7f50d609edef04dd6d522
MXerFix <maksim2003003@gmail.com>
2023-09-13 11:43:48 +0300

    email update

---
+++ email/sender.js
@@ -0,0 +1,22 @@
+const Resend = require('resend')
+
+// const resend = new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap');
+
+async function send () {
+  try {
+    const data = await new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap').emails.send({
+      from: 'info@syncsound.ru',
+      to: ['maksim2003003@gmail.com'],
+      subject: 'Hello World',
+      html: '<strong>It works!</strong>',
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  send
+}
