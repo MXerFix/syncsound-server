c2483692188aa90fa238594f0cf7b678f14c2787
MXerFix <maksim2003003@gmail.com>
2023-09-13 12:02:55 +0300

    email fix 0.3

--- email/sender.js
+++ email/sender.js
@@ -1,22 +1,22 @@
-const Resend = require('resend')
+const { Resend } = require("resend");
 
-const resend = new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap');
+const resend = new Resend("re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap");
 
-async function send () {
+async function send() {
   try {
     const data = await resend.emails.send({
-      from: 'info@syncsound.ru',
-      to: ['maksim2003003@gmail.com'],
-      subject: 'Hello World',
-      html: '<strong>It works!</strong>',
+      from: "info@syncsound.ru",
+      to: ["maksim2003003@gmail.com"],
+      subject: "Hello World",
+      html: "<strong>It works!</strong>",
     });
 
     console.log(data);
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 module.exports = {
-  send
-}
+  send,
+};
