1f4f689ca08da0ba6750a3c9670bdcfabae943dd
MXerFix <maksim2003003@gmail.com>
2023-09-13 11:58:10 +0300

    email fix 2

--- email/sender.js
+++ email/sender.js
@@ -1,10 +1,10 @@
 const Resend = require('resend')
 
-// const resend = new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap');
+const resend = new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap');
 
 async function send () {
   try {
-    const data = await new Resend('re_ZX2YDkKq_4RsqotvU4hkoaBuhfBsx3Fap').emails.send({
+    const data = await resend.emails.send({
       from: 'info@syncsound.ru',
       to: ['maksim2003003@gmail.com'],
       subject: 'Hello World',
